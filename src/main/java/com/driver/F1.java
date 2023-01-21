c9b7024fd96f527b23edda95c460c29751496ae1
jainsonm22 <jainsonm22@gmail.com>
2023-01-21 15:03:23 +0530

    assingment done 1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,13 +1,23 @@
 package com.driver;
 
 public class F1 extends Car {
-
+  int gear;
     public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+        super(name,isManual);
+        gear=0;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+
+        int newSpeed =getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,12 +29,51 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            stop();
+            setGears(1);
             //Stop the car, set gear as 1
         }
+        if(newSpeed >0 && newSpeed <= 50) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(1);
+            //Stop the car, set gear as 1
+        }
+        if(newSpeed >50 && newSpeed <= 150) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(2);
+            //Stop the car, set gear as 1
+        }
+        if(newSpeed >100 && newSpeed <= 150) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(3);
+            //Stop the car, set gear as 1
+        }
+
+        if(newSpeed >150 && newSpeed <= 200) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(4);
+            //Stop the car, set gear as 1
+        }
+
+        if(newSpeed >200 && newSpeed <= 250) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(5);
+            //Stop the car, set gear as 1
+        }
+        if(newSpeed >250 ){
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(6);
+            //Stop the car, set gear as 1
+        }
+
+
+
+
+
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
+        /*if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
-        }
+        }*/
     }
 }
