7d3ba5a56bd5642939b3cc4d9d787b66a92d9a32
jainsonm22 <jainsonm22@gmail.com>
2023-01-21 16:14:15 +0530

    assingment done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -10,8 +10,8 @@ String isManual;
     }
 
 
-    public void accelerate(int rate){
-        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate) {
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -22,48 +22,42 @@ String isManual;
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if (newSpeed == 0) {
             setGears(1);
             //Stop the car, set gear as 1
         }
-        if(newSpeed >=1 && newSpeed <= 50) {
-           setGears(1);
+        if (newSpeed >= 1 && newSpeed <= 50) {
+            setGears(1);
             //Stop the car, set gear as 1
         }
-        if(newSpeed >50 && newSpeed <= 150) {
+        if (newSpeed > 50 && newSpeed <= 100) {
             setGears(2);
             //Stop the car, set gear as 1
         }
-        if(newSpeed >100 && newSpeed <= 150) {
-           setGears(3);
+        if (newSpeed > 100 && newSpeed <= 150) {
+            setGears(3);
             //Stop the car, set gear as 1
         }
 
-        if(newSpeed >150 && newSpeed <= 200) {
+        if (newSpeed > 150 && newSpeed <= 200) {
             setGears(4);
             //Stop the car, set gear as 1
         }
 
-        if(newSpeed >200 && newSpeed <= 250) {
-          setGears(5);
+        if (newSpeed > 200 && newSpeed <= 250) {
+            setGears(5);
             //Stop the car, set gear as 1
         }
-        if(newSpeed >250 ){
+        if (newSpeed > 250) {
 
             setGears(6);
             //Stop the car, set gear as 1
         }
 
-        if(newSpeed > 0) {
+        if (newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
 
 
-
-        //for all other cases, change the gear accordingly
-
-        /*if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }*/
     }
 }
